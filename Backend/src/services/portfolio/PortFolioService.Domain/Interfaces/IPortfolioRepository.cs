41221d8f3cfc4e5c20c38775c597a8c4a7cb3bd4
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2025-12-19 17:19:44 +0330

    add new method for get portfolio by category

--- Backend/src/services/portfolio/PortFolioService.Domain/Interfaces/IPortfolioRepository.cs
+++ Backend/src/services/portfolio/PortFolioService.Domain/Interfaces/IPortfolioRepository.cs
@@ -9,5 +9,7 @@ public interface IPortfolioRepository
     Task AddAsync(Portfolio portfolio, CancellationToken ct);
     Task<List<Portfolio>> GetByDesignerIdAsync(int designerId, CancellationToken ct);
     Task<List<Portfolio>> GetRecentAsync(int count, CancellationToken ct);
+    Task<List<Portfolio>> GetByCategoryIdAsync(int categoryId, CancellationToken ct);
+
 }
 
