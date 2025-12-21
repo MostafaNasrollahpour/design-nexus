52d0fdc8802517d7250fa0ff1697c0967f41cd73
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2025-12-21 09:05:44 +0330

    add entity to app databse

---
+++ Backend/src/services/portfolio/PortFolioService.Infrastructure/Data/Migrations/20251220143405_AddDesignerProfile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PortFolioService.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDesignerProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "designerprofiles",
+                columns: table => new
+                {
+                    designer_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    bio = table.Column<string>(type: "text", nullable: true),
+                    location = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    specialty = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    avatar_url = table.Column<string>(type: "text", nullable: true),
+                    avatar_size = table.Column<long>(type: "bigint", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_designerprofiles", x => x.designer_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "designerprofiles");
+        }
+    }
+}
