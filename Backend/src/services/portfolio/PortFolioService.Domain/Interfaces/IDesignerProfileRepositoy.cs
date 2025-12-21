b6d0f7a769608b39a5b16d80ce2f4ce29bfc9f83
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2025-12-21 09:03:26 +0330

    add new interface for declare designer repository structure

---
+++ Backend/src/services/portfolio/PortFolioService.Domain/Interfaces/IDesignerProfileRepositoy.cs
@@ -0,0 +1,17 @@
+using System;
+using PortFolioService.Domain.Entities;
+
+namespace PortFolioService.Domain.Interfaces;
+
+
+public interface IDesignerProfileRepository
+{
+    Task<DesignerProfile?> GetByDesignerIdAsync(int designerId, CancellationToken ct);
+
+    Task AddAsync(DesignerProfile profile, CancellationToken ct);
+
+    Task UpdateAsync(DesignerProfile profile, CancellationToken ct);
+
+    Task SaveChangesAsync(CancellationToken ct);
+}
+
--- Backend/src/services/portfolio/PortFolioService.Domain/obj/Debug/net9.0/PortFolioService.Domain.AssemblyInfo.cs
+++ Backend/src/services/portfolio/PortFolioService.Domain/obj/Debug/net9.0/PortFolioService.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortFolioService.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15a873872baa83b073a45a887acf8bd7fa69faa0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b415e894d9dbdcaddbf7916de6316cbc7e9d48ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortFolioService.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortFolioService.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
