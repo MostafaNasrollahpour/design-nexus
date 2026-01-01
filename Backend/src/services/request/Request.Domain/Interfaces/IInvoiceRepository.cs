ad61fb81029a19514143e923cba6d1a3db16ad31
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2026-01-01 17:13:09 +0330

    change interfaces to use enums

--- Backend/src/services/request/Request.Domain/Interfaces/IInvoiceRepository.cs
+++ Backend/src/services/request/Request.Domain/Interfaces/IInvoiceRepository.cs
@@ -1,15 +1,15 @@
 using Request.Domain.Entities;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Request.Domain.Enums;
 
-namespace Request.Domain.Interfaces
+namespace Request.Domain.Interfaces;
+
+public interface IInvoiceRepository
 {
-    public interface IInvoiceRepository
-    {
-        Task<Invoice?> GetByIdAsync(int invoiceId);
-        Task<IEnumerable<Invoice>> GetByRequestIdAsync(int requestId);
-        Task<IEnumerable<Invoice>> GetByStatusAsync(string status);
-        Task AddAsync(Invoice invoice);
-        Task UpdateAsync(Invoice invoice);
-    }
+    Task<Invoice?> GetByIdAsync(int invoiceId);
+    Task<Invoice?> GetByRequestIdAsync(int requestId);
+    Task<IEnumerable<Invoice>> GetByStatusAsync(InvoiceStatus status);
+    Task AddAsync(Invoice invoice);
+    Task UpdateAsync(Invoice invoice);
 }
--- Backend/src/services/request/Request.Domain/Interfaces/IProjectRequestRepository.cs
+++ Backend/src/services/request/Request.Domain/Interfaces/IProjectRequestRepository.cs
@@ -1,16 +1,16 @@
 using Request.Domain.Entities;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Request.Domain.Enums;
 
-namespace Request.Domain.Interfaces
+namespace Request.Domain.Interfaces;
+
+public interface IProjectRequestRepository
 {
-    public interface IProjectRequestRepository
-    {
-        Task<ProjectRequest?> GetByIdAsync(int requestId);
-        Task<IEnumerable<ProjectRequest>> GetByUserIdAsync(int userId);
-        Task<IEnumerable<ProjectRequest>> GetByDesignerIdAsync(int designerId);
-        Task<IEnumerable<ProjectRequest>> GetByStatusAsync(string status);
-        Task AddAsync(ProjectRequest request);
-        Task UpdateAsync(ProjectRequest request);
-    }
+    Task<ProjectRequest?> GetByIdAsync(int requestId);
+    Task<IEnumerable<ProjectRequest>> GetByUserIdAsync(int userId);
+    Task<IEnumerable<ProjectRequest>> GetByDesignerIdAsync(int designerId);
+    Task<IEnumerable<ProjectRequest>> GetByStatusAsync(RequestStatus status);
+    Task AddAsync(ProjectRequest request);
+    Task UpdateAsync(ProjectRequest request);
 }
