945cfb02bf91533286532ed1c5e6d998dbe4d33a
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2026-01-04 09:15:13 +0330

    add docker file to gateway

--- Backend/src/services/iam/IAM.Api/obj/Debug/net9.0/IAM.Api.AssemblyInfo.cs
+++ Backend/src/services/iam/IAM.Api/obj/Debug/net9.0/IAM.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IAM.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f373d7271a887476b2677be57b2a7b094fbabe7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9524b2b313cb43937eaca5266d39be762b9c7c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("IAM.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IAM.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
