6e34caf9297001e08c9df39130f643e99b97f0bc
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2026-01-04 12:18:32 +0330

    add get project query to get project that i requested for

---
+++ Backend/src/services/request/Request.Application/Queries/GetProjectRequests/GetProjectRequestsQuery.cs
@@ -0,0 +1,10 @@
+using MediatR;
+using Request.Application.DTOs;
+
+namespace Request.Application.Queries.GetProjectRequests
+{
+    public class GetProjectRequestsQuery : IRequest<BaseResponseDto>
+    {
+        
+    }
+}
